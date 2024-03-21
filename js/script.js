a7b599d3d4519c8ffbfc5d7825b149eadf7aacde
Gustavo0916 <117117110+Gustavo0916@users.noreply.github.com>
2024-03-21 12:27:09 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/* CONFIGURÇÃO MENU MOBILE */
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+/* CONFIGURÇÃO MENU MOBILE */
+
+
+
+
+/* CONFIGURÇÃO SLIDE */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+  });
+
+  /* CONFIGURÇÃO SLIDE */
